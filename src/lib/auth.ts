cf1aa72b416dbfa554f6080a90750c2efb4c1a32
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-11-07 22:29:56 +0530

    payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+import {checkout,polar,portal} from "@polar-sh/better-auth"
+import { polarClient } from "./polar";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
@@ -8,5 +10,24 @@ export const auth = betterAuth({
     emailAndPassword:{
         enabled:true,
         autoSignIn:true,
-    }
+    },
+    plugins:[
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp:true,
+            use:[
+                checkout({
+                    products:[
+                        {
+                        productId:"13b3f73d-7c29-45c0-9dd2-6b2053fadd6e",
+                        slug:"pro",
+                        }
+                    ],
+                    successUrl:process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly:true,
+                }),
+                portal()
+            ],
+        })
+    ]
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,10 @@
 import { inngest } from "@/inngest/client";
-import { createTRPCRouter, protectedProcedure } from "../init";
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 import { TRPCError } from "@trpc/server";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
+  testAi: premiumProcedure.mutation(async () => {
 
     // throw new TRPCError({code: "BAD_REQUEST", message: "Something went wrong"})
 
