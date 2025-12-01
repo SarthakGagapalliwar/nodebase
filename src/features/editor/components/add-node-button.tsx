57e4bcb2d0398aa419bee5ac8918482f2a21eaf7
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-12-02 02:32:40 +0530

    node-selectors

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,22 +1,24 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
-import { memo } from "react"
+import { NodeSelector } from "@/components/node-selector";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { memo, useState } from "react";
 
+export const AddNodeButton = memo(() => {
+    const [selectorOpen, setSelectorOpen]=useState(false);
+  return (
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <Button
+        onClick={() => {}}
+        size="icon"
+        variant="outline"
+        className="bg-background"
+      >
+        <PlusIcon />
+      </Button>
+    </NodeSelector>
+  );
+});
 
-
-export const AddNodeButton = memo(()=>{
-    return(
-        <Button
-            onClick={()=>{}}
-            size="icon"
-            variant="outline"
-            className="bg-background"
-        >
-            <PlusIcon/>
-        </Button>
-    )
-})
-
-AddNodeButton.displayName="AddNodeButton";
+AddNodeButton.displayName = "AddNodeButton";
