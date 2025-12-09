58e47c62384236cc32e44a7fb18ace1a5c1c4dd1
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-12-09 15:43:53 +0530

    Add ingest env support

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,8 +1,17 @@
 import { Inngest } from "inngest";
 import { realtimeMiddleware } from "@inngest/realtime/middleware";
 
+const eventKey = process.env.INNGEST_EVENT_KEY;
+
+if (!eventKey) {
+  throw new Error(
+    "INNGEST_EVENT_KEY is missing. Set it in your environment before sending events."
+  );
+}
+
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "nodebase",
+  eventKey,
   middleware: [realtimeMiddleware()],
 });
